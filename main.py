b4edffcb10c10ec7d9f30a9beafa0db892967081
harshal-patil2005 <mrengineer1065@gmail.com>
2026-01-20 15:06:30 +0530

    added id in url /blog/{id}

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from fastapi import FastAPI
 # instance
 app = FastAPI()
 
-# decorator
+# decorator amd ("/") is path or route 
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
-    # function
+    # function for which u can give any name 
+
+
+@app.get("/blog/{id}")
+def show(id: int): 
+    # id must be accepted by function in order to use it inside function 
+    return {'data': id}
