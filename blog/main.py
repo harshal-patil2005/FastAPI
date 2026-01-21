050976ac2b66ea5e2b12ac7df770a1eaf66f4410
harshal-patil2005 <mrengineer1065@gmail.com>
2026-01-21 11:00:41 +0530

    the database connections is established with postgress

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from blog.schemas import Blog
+from blog import models, database
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
+
+# Create tables automatically on startup
+models.Base.metadata.create_all(bind=database.engine)
+
+@app.get("/users/")
+def read_users(db: Session = Depends(database.get_db)):
+    users = db.query(models.Blog).all()
+    return users
+
 
 @app.post("/blog")
 def create_blog(blog: Blog):
